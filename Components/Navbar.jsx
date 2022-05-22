a801409d1aee7fa67260969f85a52d6f1d281b2a
skprogrammer1007 <skprogrammer1007@gamil.com>
2022-05-22 09:37:46 +0530

    project started t

---
+++ Components/Navbar.jsx
@@ -0,0 +1,16 @@
+// module designed by himanshu patel 
+
+
+import React from 'react'
+
+// importing the bootstrap componets 
+import { Container } from 'react-bootstrap'
+
+
+export default function Navbar() {
+     return (
+          <Container>
+               <div>Navbar</div>
+          </Container>
+     )
+}
