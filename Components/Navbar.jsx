127d0fda1ab27b57a2c2294feda76fb0cb860820
skprogrammer1007 <skprogrammer1007@gamil.com>
2022-05-27 22:40:00 +0530

    semi navbar component

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,51 +1,43 @@
-// module designed by himanshu patel 
-
-
+// module designed by himanshu patel , teach module
 import React from 'react'
-
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 // importing the bootstrap componets 
-import { Container, Navbar, Nav, NavDropdown, Col, Row } from 'react-bootstrap'
 
+import { Container, Navbar, Nav, Col, Row } from 'react-bootstrap'
 
 export default function NavbarComponent() {
+     const router = useRouter()
      return (
-          <div style={{ postition: 'fixed' }}>
+          <div className="position-fixed" style={{ width: '100%', height: "100%" }}>
                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                     <Container>
-                         <Navbar.Brand href="#home">
-                              <Container>
-                                   <Col style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-                                        <Row>
-                                             S K
-                                        </Row>
-                                        <Row>
-                                             Programmer
-                                        </Row>
-                                   </Col>
-                              </Container>
+                         <Navbar.Brand >
+                              <Link href="/">
+                                   <Container>
+                                        <Col style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                                             <Row style={{ color: 'red', fontWeight: 'bolder', textShadow: '2px 2px 2px pink' }}>
+                                                  S K
+                                             </Row>
+                                             <Row sytle={{ fontWeight: 'bold', textShadow: '2px 2px 2px pink' }}>
+                                                  Programmer
+                                             </Row>
+                                        </Col>
+                                   </Container>
+                              </Link>
                          </Navbar.Brand>
                          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                          <Navbar.Collapse id="responsive-navbar-nav">
                               <Nav className="me-auto">
-                                   <Nav.Link href="#features">Features</Nav.Link>
-                                   <Nav.Link href="#pricing">Pricing</Nav.Link>
-                                   <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                        <NavDropdown.Divider />
-                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                   </NavDropdown>
+                                   <Nav.Link onClick={() => router.push('/about')}> Features</Nav.Link>
+                                   <Nav.Link >Pricing</Nav.Link>
                               </Nav>
                               <Nav>
-                                   <Nav.Link href="#deets">More deets</Nav.Link>
-                                   <Nav.Link eventKey={2} href="#memes">
-                                        Dank memes
-                                   </Nav.Link>
+                                   <Nav.Link >More deets</Nav.Link>
                               </Nav>
                          </Navbar.Collapse>
                     </Container>
                </Navbar>
-          </div>
+          </div >
      )
 }
