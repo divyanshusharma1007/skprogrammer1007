582649c8996769365e09b7b79b0e475f5f925386
skprogrammer1007 <skprogrammer1007@gamil.com>
2022-06-01 19:09:26 +0530

    footer added

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,53 +1,31 @@
 import React from 'react'
 import Image from 'next/Image'
 // import InstagramLogo from '../public/Images/InstagramLogo.webp'
-import { Table } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 export default function Footer() {
-     return (
-          <div>
-               <Table striped bordered hover size="sm">
-                    <thead>
-                         <tr>
-                              <th>#</th>
-                              <th>First Name   </th>
-                              <th>Last Name    </th>
-                              <th>Username      </th>
-                              <th>              </th>
-                              <th>             </th>
-                         </tr>
-                    </thead>
-                    <tbody>
 
-                         <tr>
-                              <td>1</td>
-                              <td>Mark</td>
-                              <td>Otto</td>
-                              <td>@mdo</td>
-                         </tr><tr>
-                              <td>1</td>
-                              <td>Mark</td>
-                              <td>Otto</td>
-                              <td>@mdo</td>
-                         </tr><tr>
-                              <td>1</td>
-                              <td>Mark</td>
-                              <td>Otto</td>
-                              <td>@mdo</td>
-                         </tr>
-                         <tr>
-                              <td>2</td>
-                              <td>Jacob</td>
-                              <td>Thornton</td>
-                              <td>@fat</td>
-                         </tr>
-                         <tr>
-                              <td>3</td>
-                              <td colSpan={2}>Larry the Bird</td>
-                              <td>@twitter</td>
-                         </tr>
-                    </tbody>
-               </Table>
-               {/* <Image source={{ InstagramLogo }} height={5} width={5} /> */}
-          </div>
+
+     const blockStyle = { color: 'white', fontWeight: 'bolder' };
+     const borderLeft = { borderLeft: '2px solid white', marginLeft: '20px' };
+
+     return (
+          <Container className="bg-dark" fluid style={blockStyle}>
+               <Row className="px-3 py-2 mx-4" >
+                    Lorem, ipsum dolor.
+               </Row>
+               <Row className="px-3 py-2 mx-4">
+                    Lorem ipsum dolor sit.
+               </Row>
+               <Row className="px-3 py-2 mx-4">
+                    <Col>
+                         lorem ipsum dolor sit
+                    </Col>
+                    <Col>
+                         <Row> <Col style={borderLeft}>hello</Col>  <Col style={borderLeft}>hello</Col>
+                              <Col style={borderLeft}>hello</Col><Col style={borderLeft}>hello</Col>
+                         </Row>
+                    </Col>
+               </Row>
+          </Container >
      )
 }
